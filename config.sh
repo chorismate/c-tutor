6e7c7bb3f5350afc00fa9a2db4f9f3b69af14482
Chorismate <me@chorismate.dev>
2023-03-22 17:56:47 -0700

    config file

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/false
+# Configuration for c-tutor
+
+# What C compiler? `cc` is normally redirected to the system C compiler,
+#                  but if you'd like to use a different one, you can set it here.
+c_compiler=cc
+
+# What text editor? `pico` is considered very beginner-friendly and
+#                   should be used by command-line newbies.
+#                   `vim` is great if you're already familiar
+#                   with the command line, but it has a learning curve.
+text_editor=pico
